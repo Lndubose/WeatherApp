8cb0016b60ca60de798c58d076484ae34b10d9bd
lndubose <lndubose13@gmail.com>
2019-01-12 16:51:15 -0600

    Create App.js
    Main area of application.

--- weather/src/App.js
+++ weather/src/App.js
@@ -5,6 +5,41 @@ import MainContianer from './components/MainContainer';
 import MainForm from './components/MainForm';
 import './App.css';
 
-class App extends React.Component {}
+class App extends React.Component {
+    state = {
+        data: {},
+    };
+
+    componentDidMount() {
+        axios
+            .get(
+                'https://api.openweathermap.org/data/2.5/weather?q=austin&APPID=172533940ab2f5b698ff4358c6d29446'
+            )
+            .then(response => this.setState({ data: response.data }))
+            .catch(err => console.log(err));
+    }
+
+    onSubmit = (event, text) => {
+        event.preventDefault();
+        axios
+            .get(
+                `https://api.openweathermap.org/data/2.5/weather?q=${text}&APPID=172533940ab2f5b698ff4358c6d29446`
+            )
+            .then(response => this.setState({ data: response.data }))
+            .catch(err => console.log('me', err));
+    };
+
+    render() {
+        console.log(this.state);
+        return (
+            <div className="App">
+                <h1>Weather app</h1>
+                <h2>What would you like to see?</h2>
+                <MainForm onSubmit={this.onSubmit} />
+                <MainContianer data={this.state.data} />
+            </div>
+        );
+    }
+}
 
 export default App;

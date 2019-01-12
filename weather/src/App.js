5e615f84eeaa50d476e4c87f8d4a39607525a3dd
lndubose <lndubose13@gmail.com>
2019-01-12 16:49:25 -0600

    Intalize project

---
+++ weather/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import axios from 'axios';
+
+import MainContianer from './components/MainContainer';
+import MainForm from './components/MainForm';
+import './App.css';
+
+class App extends React.Component {}
+
+export default App;
